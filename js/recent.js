67f84a3219ff83c069f712e3a8cc6994732a79d9
lizafaria <farializa24@gmail.com>
2022-09-09 23:28:00 +0600

    added third commit

--- js/recent.js
+++ js/recent.js
@@ -5,3 +5,20 @@ const allcategory = () => {
     .then(data => categoriesItem(data.data.news_category))
     .catch((error) => console.log(error))
 }
+
+const categoriesItem = (items) =>{
+    
+    const allcategory = document.getElementById('category');
+    items.forEach(item =>{
+        const {category_name,category_id} = item;
+        const creatLi = document.createElement('li');
+        creatLi.innerHTML = `
+        <li class="bg-info rounded me-3 py-2 px-3 ">
+        <a class="active text-white" aria-current="page" onclick="cardDetails('${category_id}')" href="#" style="text-decoration:none">${category_name}</a>
+        </li>
+        `
+        allcategory.appendChild(creatLi);
+    }
+    )
+}
+
